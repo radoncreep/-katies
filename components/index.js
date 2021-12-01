7e99b9e98e93d2b8c380da5f6dae100cb27218b5
Victor Onofiok <radoncreep@gmail.com>
2021-12-01 05:07:22 +0100

    popular_content

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from './Header';
 export { default as HorizontalFoodCard } from './HorizontalFoodCard';
+export { default as VerticalFoodCard } from './VerticalFoodCard';
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -8,7 +8,10 @@ import {
     FlatList,
     StyleSheet
 } from 'react-native';
-import { HorizontalFoodCard } from '../../components';
+import { 
+    HorizontalFoodCard, 
+    VerticalFoodCard 
+} from '../../components';
 
 import{
     FONTS,
@@ -52,17 +55,23 @@ const Home = () => {
     const [ selectedMenuType, setSelectedMenuType ] = useState(1);
     const [ menuList, setMenuList ] = useState([]);
     const [ recommends, setRecommends ] = useState([]);
+    const [ popular, setPopular ] = useState([]);
 
     useEffect(() => {
         handleChangeCategory(selectedCategoryId, selectedMenuType);
     }, [])
 
     function handleChangeCategory(categoryId, menuTypeId) {
+        // Retrieve the popular menu
+        let selectedPopular = dummyData.menu.find(({ name } ) =>  name == "Popular");
         // Retrieve the recommended menu
         let selectedRecommend = dummyData.menu.find(({ name }) => name == "Recommended");
 
         // find the menu base on the menuTypeId
         let selectedMenu = dummyData.menu.find(({ id }) => id == menuTypeId);
+
+        // Set the popular menu based on the categoryId
+        setPopular(selectedPopular?.list.filter(a => a.categories.includes(categoryId)));
 
         // Set the recommended menu based on the categoryId
         setRecommends(selectedRecommend?.list.filter(a => a.categories.includes(categoryId)));
@@ -183,6 +192,32 @@ const Home = () => {
         )
     }
 
+    function renderPopularSection() {
+        return (
+            <Section
+                title="Popular Near You"
+                onPress={() => console.log("Show all popular items")}
+            >
+                <FlatList 
+                    data={popular}
+                    keyExtractor={(item) => `${item.id}`}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item, index }) => (
+                        <VerticalFoodCard 
+                            containerStyle={{
+                                marginLeft: index == 0 ? SIZES.padding : 10,
+                                marginRight: index == popular.length - 1 ? SIZES.padding : 0
+                            }}
+                            item={item}
+                            onPress={() => console.log("Popular Vertical Card")}
+                        />
+                    )}
+                />
+            </Section>
+        )
+    }
+
     return (
         <View
             style={{
@@ -199,6 +234,9 @@ const Home = () => {
                 showsVerticalScrollIndicator={false}
                 ListHeaderComponent={
                     <View>
+                        {/* render popular section */}
+                        {renderPopularSection()}
+
                         {/* render recommended section */}
                         {renderRecommendedSection()}
                         
