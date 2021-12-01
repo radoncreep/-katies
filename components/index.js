f604a9e5c2eeb37d44945513d60c134c8dce930e
Victor Onofiok <radoncreep@gmail.com>
2021-12-01 07:26:48 +0100

    filter_modal_content_init

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as HorizontalFoodCard } from './HorizontalFoodCard';
 export { default as VerticalFoodCard } from './VerticalFoodCard';
+export { default as IconButton } from './IconButton';
